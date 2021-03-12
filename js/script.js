3e69114eae757209d1d689e937b40cb1ef6bb261
Rajat Sran <rajatsran27@gmail.com>
2021-03-13 01:02:00 +0530

    books images added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 5.2,
     slidesPerColumn: 2,
-    spaceBetween: 30,
+    spaceBetween: 20,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
