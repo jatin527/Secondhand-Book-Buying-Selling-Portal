b3c81a880e71165783216d73bc30d2bd79f10287
PearlMiglani <miglanipearl@gmail.com>
2021-03-29 10:58:48 +0530

    model created view books

--- BookPortal/homePage/models.py
+++ BookPortal/homePage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ViewBooks(models.Model):
+
+    BookName= models.CharField( max_length=200)
+    author= models.CharField( max_length=100)
+    publisher= models.models.CharField( max_length=100)
+    price= models.IntegerField()
+    image=models.FileField()
+    new= models.BooleanField()
+    userid=models.CharField( max_length=50)
