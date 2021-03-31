c2654ce501fb4983a2d5aa202ba23ae8172891fd
Shreyans Jain <shreyansjain2000@hotmail.com>
2021-03-31 14:37:10 +0530

    Completely working login

--- BookPortal/userManagement/views.py
+++ BookPortal/userManagement/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import customerUser
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import auth
 
 
 # Create your views here.
@@ -10,26 +12,34 @@ def login(request):
 
 
 def register(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         name = request.POST['Name']
         email = request.POST['Email']
         contact = request.POST['Contact']
         city = request.POST['City']
         passw = request.POST['Password']
-        user = customerUser.objects.create_user(email = email, name = name, contact_no=contact, city=city, password=passw, username=email)
+        user = customerUser.objects.create_user(
+            email=email, name=name, contact_no=contact, city=city, password=passw, username=email)
         user.save()
         return redirect('/login')
     else:
         return render(request, 'register.html')
 
+
 def login(request):
-    if request.method=='POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user = authenticate(username, password)
-        if user is not None:
-            return redirect('/')
-        else:
-            return render(request, 'loginbook.html', {"msg":"Invalid Credentials"})
+    if request.method == 'POST':
+        username = str(request.POST['username']).lower()
+        password = request.POST['password']
+        try:
+            user = customerUser.objects.get(username=username)
+            if check_password(password, user.password):
+                user = auth.authenticate(
+                    request, username=username, password=password)
+                auth.login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'loginbook.html', {'msg': 'Invalid Credentials'})
+        except Exception as e:
+            return render(request, 'loginbook.html', {'msg': 'Invalid Credentials' + str(e)})
     else:
-        return render (request, 'loginbook.html')
+        return render(request, 'loginbook.html')
