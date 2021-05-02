49e82338c4056c555a66f5140f8f9c4abf7bc60d
Shreyans Jain <shreyansjain2000@hotmail.com>
2021-05-03 00:47:39 +0530

    new individual book UI

--- BookPortal/homePage/models.py
+++ BookPortal/homePage/models.py
@@ -7,37 +7,34 @@ from django.db import models
 class ViewBooks(models.Model):
 
     BookName = models.CharField(max_length=200)
-    language=models.CharField( max_length=50)
+    language = models.CharField(max_length=50)
     author = models.CharField(max_length=100)
-    description=models.CharField(max_length=300)
-    category=models.CharField(max_length=50)
-    condition=models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    category = models.CharField(max_length=50)
+    condition = models.CharField(max_length=50)
     publisher = models.CharField(max_length=100)
     price = models.IntegerField()
     image = models.FileField(upload_to="booksImage")
     new = models.BooleanField()
     userid = models.CharField(max_length=50)
-    purchasedate= models.DateField( auto_now=False, auto_now_add=False)
+    purchasedate = models.DateField(auto_now=False, auto_now_add=False)
+
 
 class Cart(models.Model):
 
     id_book = models.IntegerField()
-    id_user= models.IntegerField()
+    id_user = models.IntegerField()
+
 
 class Orders(models.Model):
 
     id_book = models.IntegerField()
-    id_user= models.IntegerField()
-    status= models.CharField()
-    purchasedate= models.DateField( auto_now=True, auto_now_add=True)
+    id_user = models.IntegerField()
+    status = models.CharField(max_length=100)
+    purchasedate = models.DateField(auto_now=True)
+
 
 class Review(models.Model):
 
-    id_user= models.IntegerField()
-    audio= models.FileField(upload_to="audio")
-
-
-
-    
-
-
+    id_user = models.IntegerField()
+    audio = models.FileField(upload_to="audio")
--- BookPortal/homePage/views.py
+++ BookPortal/homePage/views.py
@@ -13,24 +13,24 @@ def home(request):
 
 def sellBooks(request):
     if request.user.is_authenticated:
-            if request.method == 'POST':
-                authorName = request.POST['authorname']
-                bookname = request.POST['bookname']
-                blang = request.POST['blang']
-                category = request.POST['category']
-                date = '2021-03-30'
-                price = request.POST['price']
-                img = request.FILES['img']
-                bdis = request.POST['bdis']
-                bdistdet = request.POST['bdisdet']
-                book = ViewBooks(BookName=bookname, language=blang, author=authorName, description=bdistdet, category=category,
-                                condition=bdis, price=price, image=img, new=True, userid=request.user.email, purchasedate=date, publisher='testing123')
-                book.save()
-                return redirect('/')
-            else:
-                return render(request, 'sell_books.html')
+        if request.method == 'POST':
+            authorName = request.POST['authorname']
+            bookname = request.POST['bookname']
+            blang = request.POST['blang']
+            category = request.POST['category']
+            date = '2021-03-30'
+            price = request.POST['price']
+            img = request.FILES['img']
+            bdis = request.POST['bdis']
+            bdistdet = request.POST['bdisdet']
+            book = ViewBooks(BookName=bookname, language=blang, author=authorName, description=bdistdet, category=category,
+                             condition=bdis, price=price, image=img, new=True, userid=request.user.email, purchasedate=date, publisher='testing123')
+            book.save()
+            return redirect('/')
+        else:
+            return render(request, 'sell_books.html')
     else:
-        return render(request, 'errorpage.html', {'msg':'You are not logged in'})
+        return render(request, 'errorpage.html', {'msg': 'You are not logged in'})
 
 
 def viewBook(request):
@@ -38,7 +38,7 @@ def viewBook(request):
     book = ViewBooks.objects.get(id=id)
     print(id)
     print(book.BookName)
-    return render(request, 'viewBooks.html', {'book':book})
+    return render(request, 'bookinfo.html', {'book': book})
 
 
 def login(request):
